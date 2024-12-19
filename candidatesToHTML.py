7165444305710dc66f4053240261a9e51cf16f55
Jessica O'Brien <z5479430@ad.unsw.edu.au>
2024-12-19 23:20:45 +1100

    turning vote input sections into table (+ automating HTML code creation)

---
+++ candidatesToHTML.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+
+if len(sys.argv) < 2:
+    print(f'Usage: {sys.argv[0]} <file.csv>')
+    sys.exit(1)
+
+# assumes file format: state, division (electorate name), ballot position, surname, given name, party name
+csv_file = sys.argv[1]
+command = f"cat {csv_file} | grep '^NSW,Bradfield' | cut -d',' -f3,4,5,6 | sort -n | cut -d',' -f2,3,4 >tempcandidates.csv"
+candidateData = subprocess.run(command, shell = True, executable="/bin/dash")
+
+htmlCode = '''<table id="resultInputs">
+    <thead>
+        <tr>
+            <th>Party</th>
+            <th>Candidate</th>
+            <th>First Preference</th>
+            <th>TCP to Boele</th>
+            <th>TCP to Liberal</th>
+        </tr>
+    </thead>
+    <tbody>'''
+
+with open('tempcandidates.csv') as file:
+    for line in file:
+        values = line.split(',')
+        party = values[2].strip()
+        name = f'{values[0]}, {values[1]}'
+        htmlCode += f'''
+        <tr>
+            <th>{party}</th>
+            <th>{name}</th>
+            <th><input name="firstPreference" placeholder="Votes" type="number" /></th>
+            <th><input name="BoeleTCP" placeholder="Votes" type="number" /></th>
+            <th><input name="LiberalTCP" placeholder="Votes" type="number" /></th>
+        </tr>'''
+
+htmlCode += '''
+    </tbody>
+</table>'''
+
+print(htmlCode)
